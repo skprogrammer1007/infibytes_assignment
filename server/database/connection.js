452dee503430a15c2923bc7dd03aaf83434b74ec
diyvanshu <skprogrammer1007@gmail.com>
2022-12-22 05:41:27 +0530

    update

--- server/database/connection.js
+++ server/database/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 const connectToMongo = async () => {
-    await mongoose.connect("mongodb+srv://eassyway22:akku5678@cluster0.wm07ztk.mongodb.net/?retryWrites=true&w=majority", () => { console.log("connection is successfull") })
+    await mongoose.connect(process.env.mongouri, () => { console.log("connection is successfull") })
 }
 module.exports = connectToMongo;
